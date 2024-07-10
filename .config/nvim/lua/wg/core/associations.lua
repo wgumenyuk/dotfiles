cdc814ac2d6230fd48cbbb2e878b945e6d008bcf
wgumenyuk <wlad@gumenyuk.de>
2024-07-10 18:10:32 +0200

    refactor: remove `wg` directory and files

--- .config/nvim/lua/wg/core/associations.lua
+++
@@ -1,8 +0,0 @@
-local associations = {
-  ["*.swcrc"] = "json",
-  [".env*"] = "sh"
-}
-
-for k, v in pairs(associations) do
-  vim.cmd("autocmd BufRead,BufNewFile " .. k .. " set filetype=" .. v)
-end
--- .config/nvim/lua/wg/core/init.lua
+++
@@ -1,3 +0,0 @@
-require("wg.core.associations")
-require("wg.core.keymap")
-require("wg.core.options")
