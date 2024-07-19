de3adde1857e4cdf118b433d129633f17321a56e
wgumenyuk <wlad@gumenyuk.de>
2024-07-19 21:14:13 +0200

    feat: add `treesitter` plugin

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      sync_install = false,
+      highlight = {
+        enable = true,
+        use_languagetree = true
+      },
+      indent = {
+        enable = true
+      },
+      autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+        filetypes = {
+          "html",
+          "svelte"
+        }
+      },
+      ensure_installed = {
+        "bash",
+        "css",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "svelte",
+        "tmux",
+        "typescript",
+        "tsx",
+        "vim",
+        "yaml"
+      }
+    })
+  end
+}
