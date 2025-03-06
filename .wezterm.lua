4bbdfc307a8348b4122ac9c364676e0b6f330eda
wgumenyuk <wlad@gumenyuk.de>
2025-03-06 11:18:35 +0100

    feat: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,62 @@
+local wezterm = require("wezterm")
+
+function get_os()
+	local target = wezterm.target_triple
+
+	if target:find("windows") then
+		return "windows"
+	end
+
+	if target:find("linux") then
+		return "linux"
+	end
+
+	return nil
+end
+
+return {
+	-- Shell
+	default_prog = get_os() == "windows" and { "powershell.exe" } or { "bash" },
+
+	-- Theme
+	color_scheme = "zenwritten_dark",
+	font = wezterm.font_with_fallback({
+		{
+			family = "JetBrainsMono Nerd Font Mono",
+			harfbuzz_features = {
+				"zero"
+			}
+		},
+		"Noto Color Emoji",
+		"Segoe UI Emoji"
+	}),
+	font_rules = {
+		{
+			intensity = "Half",
+			font = wezterm.font({
+				family = "JetBrainsMono Nerd Font Mono",
+				weight = "Regular"
+			})
+		}
+	},
+	font_size = 14.0,
+
+	-- Window
+	initial_cols = 100,
+	initial_rows = 30,
+	enable_tab_bar = false,
+
+	-- Keybinds
+	keys = {
+		{
+			key = "F11",
+			action = wezterm.action.ToggleFullScreen
+		}
+	},
+
+	-- Other
+	audible_bell = "Disabled",
+	window_close_confirmation = "NeverPrompt",
+	hide_mouse_cursor_when_typing = false,
+	detect_password_input = false
+}
